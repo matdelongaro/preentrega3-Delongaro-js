2ccb388097e64e9240fb2412141aceb8d1aa1f9c
matias delongaro <matiasadelongaro@gmail.com>
2024-04-02 19:04:28 -0300

    styles

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const formulario = document.querySelector('#formulario');
+const listaDeudas = document.querySelector('#lista-deudas')
 let deudas = []
 
 function mostrarError(msj){
@@ -18,7 +19,55 @@ function agregardeuda(evt) {
     const deuda = document.querySelector("#deuda").value;
     deuda === '' && mostrarError("No puede ingresar un item vacío")
     
+    const objDeuda = {
+        id: Date.now(),
+        texto: deuda
+    }
+    
+    deudas.push(objDeuda);
+    formulario.reset();
+    createHTML();
     
 }
 
+function createHTML(){
+    clearDeudas();
+    deudas.forEach(item => {
+        const btnClear = document.createElement('a');
+        btnClear.classList.add('limpiar-comentario');
+        btnClear.textContent= '❌'
+        const li = document.createElement('li');
+        li.textContent = item.texto;
+        li.setAttribute('id', item.id)
+        li.appendChild(btnClear)
+        listaDeudas.appendChild(li)
+    })
+
+    sincronizarStorage()
+}
+
+function sincronizarStorage(){
+    localStorage.setItem('deudas', JSON.stringify(deudas))
+}
+
+function clearDeudas(){
+    while(listaDeudas.firstChild){
+        listaDeudas.removeChild(listaDeudas.firstChild)
+    }
+}
+
+function borrarItem(evt){
+    evt.preventDefault()
+    console.log(evt.target.parentElement.id)
+    const id = evt.target.parentElement.id;
+    
+    deudas = deudas.filter(deuda => deuda.id != id);
+    createHTML()
+}
 formulario.addEventListener('submit' , agregardeuda);
+listaDeudas.addEventListener('click', borrarItem);
+
+document.addEventListener('DOMContentLoaded', () => {
+    deudas = JSON.parse(localStorage.getItem('deudas')) || []
+    createHTML()
+} )
